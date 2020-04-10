be68678efa6b71e04aa96411f54a3a9b2f05eb90
George Saieed <saieed.george0@gmail.com>
2020-04-10 18:05:21 -0400

    update

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
             if (value["Link to Image"] == null) {
                 image = "";
             }
-            if (link == "N/a") {
+            if (link == "N/a" || link == null) {
                 link = "#";
                 blank = "";
             }
